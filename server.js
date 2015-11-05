2416e1bee0574d32159483472ef90c19fe9db14b
Matthieu LEMOINE <matthieu.lemoine@smile.fr>
2015-11-05 15:01:03 +0100

    Fix filename

--- server.js
+++ server.js
@@ -21,7 +21,15 @@ var dbConfig = require('./db.js');
 var mongoose = require('mongoose');
 mongoose.connect(dbConfig.url);
 require('./models/Posts');
-var upload = multer({ dest: 'public/uploads/' })
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now()+"-"+file.originalname)
+  }
+});
+var upload = multer({ storage : storage });
 
 /**
  * Configuration
