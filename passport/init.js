a1b0b9a6134d209229a31f89fa529cbe4337b9d7
matthieu <ml.94230@gmail.com>
2015-11-23 08:48:31 +0100

    Clean done

--- passport/init.js
+++ passport/init.js
@@ -1,7 +1,7 @@
-var login    = require('./login');
-var signup   = require('./signup');
-var mongoose = require('mongoose');
-var User     = mongoose.model('Users');
+var login = require('./login');
+var signup = require('./signup');
+var mongoose= require('mongoose');
+var User = mongoose.model('Users');
 
 module.exports = function(passport){
 	// Passport needs to be able to serialize and deserialize users to support persistent login sessions
