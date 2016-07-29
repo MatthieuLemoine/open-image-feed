977406ed1ce603915aa14ff0222635cc7e8918f4
MatthieuLemoine <ml.94230@gmail.com>
2016-07-29 23:45:43 +0200

    Reducers tests

--- src/front/app/app/reducers/__tests__/reducers.js
+++ src/front/app/app/reducers/__tests__/reducers.js
@@ -1,5 +1,158 @@
-describe('app reducers', () => {
-  it('have no test', () => {
+jest.unmock('../reducers');
+jest.unmock('../../../users/actions/login');
+jest.unmock('../../../users/actions/signup');
+jest.unmock('../../../posts/actions/posts');
+jest.unmock('../../../posts/actions/likes');
+jest.unmock('../../../posts/actions/comments');
+jest.unmock('../../../posts/reducers/reducers');
+jest.unmock('../../../users/reducers/reducers');
 
+import deepFreeze from 'deep-freeze';
+import rootReducer, {
+  message,
+  getPosts,
+  hasMorePosts,
+  getOffset,
+  hasErrorAddPost,
+  hasErrorGetPosts,
+  hasErrorLike,
+  hasErrorComment,
+  isFetchingPosts,
+  isFetchingCount,
+  isPersistingPost,
+  isLiking,
+  isCommenting,
+  getUser,
+  isLogged,
+  hasErrorLogin,
+  hasErrorSignup,
+  isFetchingUser
+} from '../reducers';
+import { ERROR_LOGIN, SUCCESS_LOGIN } from '../../../users/actions/login';
+import { ERROR_SIGNUP, SUCCESS_SIGNUP } from '../../../users/actions/signup';
+import { ERROR_ADD_POST, ERROR_GET_POSTS, SUCCESS_ADD_POST } from '../../../posts/actions/posts';
+import { ERROR_LIKE } from '../../../posts/actions/likes';
+import { ERROR_COMMENT, SUCCESS_COMMENT } from '../../../posts/actions/comments';
+
+describe('Root reducer', () => {
+  [
+    ERROR_LOGIN,
+    SUCCESS_LOGIN,
+    ERROR_SIGNUP,
+    SUCCESS_SIGNUP,
+    ERROR_ADD_POST,
+    ERROR_GET_POSTS,
+    SUCCESS_ADD_POST,
+    ERROR_LIKE,
+    ERROR_COMMENT,
+    SUCCESS_COMMENT
+  ].forEach((type) => {
+    it(`should update state with message on ${type} and be pure`, () => {
+      const state = {
+        root : {
+          message : `MESSAGE_${type}`
+        }
+      };
+      const action = {
+        type,
+        message : `ERROR_${type}`
+      };
+      // Root reducer should be pure
+      deepFreeze(state);
+      deepFreeze(action);
+      const newState = rootReducer(state, action);
+      expect(newState.root.message).toEqual(action.message);
+    });
+  });
+
+  const rootState = {
+    root : {
+      message : 'junk'
+    },
+    post : {
+      errorAddPost    : false,
+      errorGetPosts   : false,
+      errorLike       : true,
+      errorComment    : true,
+      isFetching      : false,
+      isFetchingCount : false,
+      isLiking        : true,
+      isCommenting    : false,
+      isPersisting    : false,
+      isWatched       : true,
+      offset          : 1,
+      posts           : [
+        {
+          id : 1,
+          title : 'Junk Title'
+        }
+      ],
+      totalPosts      : 12
+    },
+    user : {
+      isFetching : false,
+      isLogged   : true,
+      user       : {
+        authHeader : 'Basic DSKF?LKDF?LDSFK?SFqsldm',
+        password   : 'password',
+        username   : 'username'
+      }
+    }
+  };
+  deepFreeze(rootState);
+
+  it('should return message', () => {
+    expect(message(rootState)).toEqual(rootState.root.message);
+  });
+  it('should return posts', () => {
+    expect(getPosts(rootState)).toEqual(rootState.post.posts);
+  });
+  it('should return hasMorePosts', () => {
+    expect(hasMorePosts(rootState)).toEqual(rootState.post.totalPosts > rootState.post.offset);
+  });
+  it('should return offset', () => {
+    expect(getOffset(rootState)).toEqual(rootState.post.offset);
+  });
+  it('should return hasErrorAddPost', () => {
+    expect(hasErrorAddPost(rootState)).toEqual(rootState.post.errorAddPost);
+  });
+  it('should return hasErrorGetPosts', () => {
+    expect(hasErrorGetPosts(rootState)).toEqual(rootState.post.errorGetPosts);
+  });
+  it('should return hasErrorLike', () => {
+    expect(hasErrorLike(rootState)).toEqual(rootState.post.errorLike);
+  });
+  it('should return hasErrorComment', () => {
+    expect(hasErrorComment(rootState)).toEqual(rootState.post.errorComment);
+  });
+  it('should return isFetchingPosts', () => {
+    expect(isFetchingPosts(rootState)).toEqual(rootState.post.isFetching);
+  });
+  it('should return isFetchingCount', () => {
+    expect(isFetchingCount(rootState)).toEqual(rootState.post.isFetchingCount);
+  });
+  it('should return isPersistingPost', () => {
+    expect(isPersistingPost(rootState)).toEqual(rootState.post.isPersisting);
+  });
+  it('should return isLiking', () => {
+    expect(isLiking(rootState)).toEqual(rootState.post.isLiking);
+  });
+  it('should return isCommenting', () => {
+    expect(isCommenting(rootState)).toEqual(rootState.post.isCommenting);
+  });
+  it('should return user info', () => {
+    expect(getUser(rootState)).toEqual(rootState.user.user);
+  });
+  it('should return isLogged', () => {
+    expect(isLogged(rootState)).toEqual(rootState.user.isLogged);
+  });
+  it('should return hasErrorLogin', () => {
+    expect(hasErrorLogin(rootState)).toEqual(rootState.user.errorLogin);
+  });
+  it('should return hasErrorSignup', () => {
+    expect(hasErrorSignup(rootState)).toEqual(rootState.user.errorSignup);
+  });
+  it('should return isFetchingUser', () => {
+    expect(isFetchingUser(rootState)).toEqual(rootState.user.isFetching);
   });
 });
--- src/front/app/users/reducers/__tests__/reducers.js
+++ src/front/app/users/reducers/__tests__/reducers.js
@@ -1,5 +1,77 @@
-describe('users reducers', () => {
-  it('have no test', () => {
+jest.unmock('../reducers');
+jest.unmock('../../actions/login');
+jest.unmock('../../actions/signup');
 
+import deepFreeze from 'deep-freeze';
+import userReducer, {
+  getUser,
+  isLogged,
+  hasErrorLogin,
+  hasErrorSignup,
+  isFetching
+} from '../reducers';
+import { REQUEST_LOGIN, SUCCESS_LOGIN, ERROR_LOGIN } from '../../actions/login';
+import { REQUEST_SIGNUP, SUCCESS_SIGNUP, ERROR_SIGNUP } from '../../actions/signup';
+
+describe('User reducer', () => {
+  const userState = {
+    isFetching     : false,
+    isLogged       : true,
+    hasErrorLogin  : false,
+    hasErrorSignup : false,
+    user           : {
+      authHeader : 'Basic DSKF?LKDF?LDSFK?SFqsldm',
+      password   : 'password',
+      username   : 'username'
+    }
+  };
+  deepFreeze(userState);
+  it('should return user', () => {
+    expect(getUser(userState)).toEqual(userState.user);
+  });
+  it('should return isLogged', () => {
+    expect(isLogged(userState)).toEqual(userState.isLogged);
+  });
+  it('should return isFetching', () => {
+    expect(isFetching(userState)).toEqual(userState.isFetching);
+  });
+  it('should return hasErrorSignup', () => {
+    expect(hasErrorSignup(userState)).toEqual(userState.errorSignup);
+  });
+  it('should return hasErrorLogin', () => {
+    expect(hasErrorLogin(userState)).toEqual(userState.errorLogin);
+  });
+
+  it('should set fetching on login request', () => {
+    expect(userReducer(userState, { type : REQUEST_LOGIN }).isFetching).toBe(true);
+  });
+  it('should reset error and fetching, set isLogged and save user credentials on login success',
+    () => {
+      const state = userReducer(userState,  { type : SUCCESS_LOGIN, user : userState.user });
+      expect(state.isFetching).toBe(false);
+      expect(state.errorLogin).toBe(false);
+      expect(state.user).toEqual(userState.user);
+      expect(state.isLogged).toBe(true);
+    });
+  it('should set error and reset fetching on login error', () => {
+    const state = userReducer(userState,  { type : ERROR_LOGIN });
+    expect(state.isFetching).toBe(false);
+    expect(state.errorLogin).toBe(true);
+  });
+  it('should set fetching on signup request', () => {
+    expect(userReducer(userState, { type : REQUEST_SIGNUP }).isFetching).toBe(true);
+  });
+  it('should reset error and fetching, set isLogged and save user credentials on signup success',
+    () => {
+      const state = userReducer(userState,  { type : SUCCESS_SIGNUP, user : userState.user });
+      expect(state.isFetching).toBe(false);
+      expect(state.errorSignup).toBe(false);
+      expect(state.user).toEqual(userState.user);
+      expect(state.isLogged).toBe(true);
+    });
+  it('should set error and reset fetching on signup error', () => {
+    const state = userReducer(userState,  { type : ERROR_SIGNUP });
+    expect(state.isFetching).toBe(false);
+    expect(state.errorSignup).toBe(true);
   });
 });
