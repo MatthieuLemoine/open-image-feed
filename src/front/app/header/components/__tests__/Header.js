837502effb31581b8264df4e75791e45a1e06b49
Matthieu LEMOINE <matthieu.lemoine@smile.fr>
2016-07-29 14:28:15 +0200

    Header test

---
+++ src/front/app/header/components/__tests__/Header.js
@@ -0,0 +1,31 @@
+jest.unmock('../Header.jsx');
+
+import { shallow } from 'enzyme';
+import Header from '../Header.jsx';
+
+describe('Header', () => {
+  it('should hide add-post button when not logged in', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('#add-post')).toHaveStyle('display', 'none');
+  });
+  it('should display login button when not logged in', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('login')).toHaveStyle('display', 'initial');
+  });
+  it('should display add-post button when logged in', () => {
+    const wrapper = shallow(<Header isLogged />);
+    expect(wrapper.find('#add-post')).toHaveStyle('display', 'initial');
+  });
+  it('should hide login button when logged in', () => {
+    const wrapper = shallow(<Header isLogged />);
+    expect(wrapper.find('login')).toHaveStyle('display', 'none');
+  });
+  it('should display progress indicator when fetching', () => {
+    const wrapper = shallow(<Header isFetching />);
+    expect(wrapper.find('.progress-indicator')).toHaveStyle('display', 'block');
+  });
+  it('should hide progress indicator when not fetching', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('.progress-indicator')).toHaveStyle('display', 'none');
+  });
+});
