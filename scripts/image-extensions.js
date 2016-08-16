4f5cf8196a796eb20b001181743cfb1e626c15c4
Matthieu LEMOINE <matthieu.lemoine@smile.fr>
2016-08-16 11:50:06 +0200

    Add image extensions

---
+++ scripts/image-extensions.js
@@ -0,0 +1,35 @@
+const Post  = require('../server/database/schema').Post;
+const fs    = require('fs');
+const mmm   = require('mmmagic');
+const Magic = mmm.Magic;
+const ROOT_PATH = `${__dirname}/../dist`;
+
+Post
+  .run()
+  .then(posts => {
+    posts.forEach(post => {
+      const imageName = post.image;
+      // If does not have an extension
+      if (imageName.indexOf('.') === -1) {
+        const magic = new Magic(mmm.MAGIC_MIME_TYPE);
+        const filePath = `${ROOT_PATH}${imageName}`;
+        magic.detectFile(filePath, (err, result) => {
+          if (err) throw err;
+          if (result.split('/').length > 1) {
+            const type = result.split('/')[1];
+            const newFilePath = `${ROOT_PATH}${imageName}.${type}`;
+            post.image = `${imageName}.${type}`;
+            process.stdout.write(`${post.image}\n`);
+            fs.rename(filePath, newFilePath, (error) => {
+              if (error) throw error;
+              post
+                .save()
+                .then(doc => {
+                  process.stdout.write(`${doc.title} updated\n`);
+                });
+            });
+          }
+        });
+      }
+    });
+  });
--- server/utils/upload.js
+++ server/utils/upload.js
@@ -5,7 +5,14 @@ const uuid   = require('node-uuid');
 
 const storage = multer.diskStorage({
   destination : (req, file, cb) => cb(null, `${__dirname}/../../dist/uploads/`),
-  filename : (req, file, cb) => cb(null, uuid.v4())
+  filename : (req, file, cb) => {
+    let name = uuid.v4();
+    if (file.mimetype && file.mimetype.split('/').length > 1) {
+      const type = file.mimetype.split('/')[1];
+      name += `.${type}`;
+    }
+    cb(null, name);
+  }
 });
 const upload = multer({ storage });
 
